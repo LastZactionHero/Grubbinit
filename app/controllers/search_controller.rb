fcc1b4e1153acd5fa9ed0b8ac1bd936e9da39907
System Administrator <root@Macintosh-3.local>
2010-04-17 17:51:16 -0500

    Corrected search algorithm

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -11,12 +11,26 @@ class SearchController < ApplicationController
 	  @search_zip = value.to_s if key.to_s == "zipValue"
 	end
 	
+	search_terms = @search_term.split
+	
 	# Select restaurant list
-	@restaurants = Restaurant.all.select{ |restaurant| 
-		( restaurant.name.downcase.include? @search_term.downcase and !@search_term.empty? ) or
-		( restaurant.description.downcase.include? @search_term.downcase and !@search_term.empty? ) or
-		( restaurant.address.downcase.include? @search_zip.downcase and !@search_zip.empty? )
-		}
+	@restaurants
+	
+	# Just search term, no zip
+	if !@search_term.empty? and @search_zip.empty?
+	  @restaurants = Restaurant.all.select{ |restaurant| doesRestaurantContainSearch( restaurant, search_terms ) == :true }
+	# Just zip, no search term
+	elsif @search_term.empty? and !@search_zip.empty?
+	  @restaurants = Restaurant.all.select{ |restaurant| restaurant.address.downcase.include? @search_zip.downcase }
+	# Search term and zip
+	else !@search_term.empty? and !@search_zip.empty?
+	  @restaurants = Restaurant.all.select{ |restaurant| 
+			doesRestaurantContainSearch( restaurant, search_terms ) == :true and
+			restaurant.address.downcase.include? @search_zip.downcase
+			}
+	end
+	
+
   end
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,16 @@
 class Restaurant < ActiveRecord::Base
   has_many :menu_items
+
+  def self.doesRestaurantContainSearch( restaurant, search_terms )
+    ret = :true
+  
+    search_terms.each do |term|
+      if !restaurant.name.downcase.include? term.downcase and !restaurant.description.downcase.include? term.downcase
+	    ret = :false
+	  end
+    end
+  
+    return ret
+  end
+  
 end
